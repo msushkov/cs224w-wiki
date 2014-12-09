0b406e4d19a3e2130bafd89e236cfb1fc0e6d817
Mikhail Sushkov <sushkovm@gmail.com>
2014-12-08 18:48:50 -0800

    comment ontology dist

--- code/util.py
+++ code/util.py
@@ -193,9 +193,9 @@ def get_features(article1_name, article2_name, article1_words, article2_words, n
 
     # feature 4: ontology distance (length of path through LCA)
     # feature 5: ontology height of LCA
-    (feat4, feat5) = get_ontology_distance(article1_name, article2_name, name_to_type, type_to_depth, type_to_node)
-    features.append(feat4)
-    features.append(feat5)
+    # (feat4, feat5) = get_ontology_distance(article1_name, article2_name, name_to_type, type_to_depth, type_to_node)
+    # features.append(feat4)
+    # features.append(feat5)
 
     return features
 
